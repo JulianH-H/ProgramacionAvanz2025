bdbf22487cefe1c241ef712ccef822db49b5e4cd
Julian H <jeholguinr@udistrital.edu.co>
2025-09-17 15:40:28 -0500

    Update Diccionarios2_Commented.py
    Se actualizo avance

--- Miscelaneo/Diccionarios2_Commented.py
+++ Miscelaneo/Diccionarios2_Commented.py
@@ -34,7 +34,7 @@ key_to_check = "Landmark 81" #Primero asignamos la llave de busqueda a otro obje
 if key_to_check in building_heights:
   print(building_heights["Landmark 81"])
   #Y al usar este if nos presentara con un valor booleano si esta o no
-
+print("")
 #Otra manera es agregar preeventivamente un valor dummy, ejemplo con zodiac_elements
 
 zodiac_elements["energy"] = "Not a Zodiac element"
@@ -42,5 +42,35 @@ zodiac_elements["energy"] = "Not a Zodiac element"
 
 if "energy" in zodiac_elements:
   print(zodiac_elements["energy"])
-
+print("")
 #Podemos presentarle al usuario si existe o no  
+#Asi mismo podemos llamar con la llave usando .get
+
+
+building_heights.get("Shanghai Tower") #Esta linea retornara: 632:
+building_heights.get("My House") #Esta linea retornara: None
+print("")
+
+
+#Para evitar devolver none en un contexto donde estemos 
+# usando ids de cuentas podemos usar un if, tal que
+
+user_ids = {"teraCoder": 100019, "pythonGuy": 182921, "samTheJavaMaam": 123112, "lyleLoop": 102931, "keysmithKeith": 129384}
+user_ids.get("teraCoder")
+#En este caso verificamos que teraCoder Exista
+
+if user_ids.get("teraCoder") == None:
+   tc_id = 1000
+   #De no estarlo, imprimiriamos 1000 en vez de none
+else: 
+   tc_id = user_ids.get("teraCoder")
+    #De lo contrario, imprimimos su user
+print(tc_id)
+
+if user_ids.get("superStackSmash") == None:
+     stack_id = 100000
+    #Lo mismo para este caso, donde imprimiremos 100000
+print(stack_id)
+
+##  Eliminar llaves
+
